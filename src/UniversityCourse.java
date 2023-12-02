c627ec3528c4499dee736e12543698b833e82e3d
RagadAlShaboul <ragad.alshaboul@gmail.com>
2023-12-02 20:25:19 +0300

    Project uploaded by Sr.Hamza

---
+++ src/UniversityCourse.java
@@ -0,0 +1,34 @@
+import java.sql.Array;
+import java.util.ArrayList;
+
+public class UniversityCourse {
+    private  String courseName;
+    private String instructorName;
+    private ArrayList<String> studentNames = new ArrayList<String>();
+    public UniversityCourse(String courseName,String instructorName){
+        this.courseName = courseName;
+        this.instructorName = instructorName;
+    }
+    public void register(String studentName){
+        if(studentNames == null){
+            throw new IllegalArgumentException();
+        }
+        this.studentNames.add(studentName);
+    }
+    public void register(String[] studentNames){
+        if(studentNames == null){
+            throw new IllegalArgumentException();
+        }
+        for(int i=0;i<studentNames.length;i++){
+            this.studentNames.add(studentNames[i]);
+        }
+    }
+
+    public boolean isRegistered(String studentName){
+        return this.studentNames.contains(studentName);
+    }
+    public int getNumberOfRegisteredStudents(){
+        return this.studentNames.size();
+    }
+
+}
